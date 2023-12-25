59d9ea381ad20b3573a3b2c71d79755db6f83e39
mrcro <mr.crossfade.mnr@gmail.com>
2023-12-25 20:34:38 +0100

    prep for Railway

--- ankietaProject/settings.py
+++ ankietaProject/settings.py
@@ -23,16 +23,7 @@ load_dotenv(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-p=htcq9#@6#a69_uw(lwz8d%i=cx8oc*5(*h(kx%2f1(g*3ree'
-#SECRET_KEY = os.getenv('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-#DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
-#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
-#CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(' ')
-#SECURE_SSL_REDIRECT = \
-#    os.getenv('SECURE_SSL_REDIRECT', '0').lower() in ['true', 't', '1']
-#if SECURE_SSL_REDIRECT:
-#    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 
@@ -53,6 +44,7 @@ SESSION_COOKIE_AGE = 150
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -90,11 +82,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-#       'NAME': os.environ.get('DBNAME'),
-#       'HOST': os.environ.get('DBHOST'),
-#       'USER': os.environ.get('DBUSER'),
-#       'PASSWORD': os.environ.get('DBPASS'),
-#       'OPTIONS': {'sslmode': 'require'},
     }
 }
 
@@ -133,7 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
