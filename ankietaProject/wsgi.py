8a888b8c6f02572b9342796f8ea3d4436c6d6ff0
mrcro <mr.crossfade.mnr@gmail.com>
2023-12-26 11:06:52 +0100

    wsgi

--- ankietaProject/wsgi.py
+++ ankietaProject/wsgi.py
@@ -19,9 +19,9 @@ application = WhiteNoise(application)
 path = '/mrcro/PycharmProjects/ankietaProject'
 if path not in sys.path:
     sys.path.insert(0, path)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ankietaProject.settings')
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ankietaProject.settings')
 
 application = get_wsgi_application()
