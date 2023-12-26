dd63dbfd070c9ae2f422a5cd289b1790deeb99bc
mrcro <mr.crossfade.mnr@gmail.com>
2023-12-26 10:48:16 +0100

    wsgi change

--- ankietaProject/wsgi.py
+++ ankietaProject/wsgi.py
@@ -10,6 +10,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 import sys
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+
+application = get_wsgi_application()
+application = WhiteNoise(application)
+
 path = '/mrcro/PycharmProjects/ankietaProject'
 if path not in sys.path:
     sys.path.insert(0, path)
