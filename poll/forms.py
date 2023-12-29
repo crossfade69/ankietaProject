08bd62c52bcbe62967a97f5f10bab3de976ae082
mrcro <mr.crossfade.mnr@gmail.com>
2023-12-29 21:42:07 +0100

    usunąłem nie potrzebny form

--- poll/forms.py
+++ poll/forms.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
 from django.forms import ModelForm
-#from django.contrib.auth.forms import PasswordChangeForm
 from .models import Poll
 
 
